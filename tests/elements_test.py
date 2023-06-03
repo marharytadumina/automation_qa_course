a966b6edae0ea79838f3826d0d662b88b89dd417
Margo_Vodzi <margarita.vodzinskaya@ilogos.biz>
2023-06-03 16:12:15 -0400

    6_Lesson

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,5 +1,5 @@
 import time
-from pages.elements_page import TextBoxPage, CheckBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage
 
 
 class TestElements:
@@ -25,3 +25,17 @@ class TestElements:
             output_result = check_box_page.get_output_result()
             assert input_checkbox == output_result, "Input text and output checkbox is not equal"
 
+    class TestRadiobutton:
+
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
+            radio_button_page.open()
+            radio_button_page.click_on_the_radio_button('yes')
+            output_yes = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('impressive')
+            output_impressive = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('no')
+            output_no = radio_button_page.get_output_result()
+            assert output_yes == 'Yes', "'Yes' have not been selected"
+            assert output_impressive == 'Impressive', "'Impressive' have not been selected"
+            assert output_no == 'No', "'No' have not been selected"
