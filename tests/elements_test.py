920b38b69de1a7edda82820355e9c4a0dd07d5f1
Margo_Vodzi <margarita.vodzinskaya@ilogos.biz>
2023-06-03 15:08:42 -0400

    5.3_lesson

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,5 +1,5 @@
 import time
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
@@ -14,3 +14,14 @@ class TestElements:
             assert email == output_email, "the email does not match"
             assert current_address == output_cur_addr, "the current address does not match"
             assert permanent_address == output_per_addr, "the permanent address does not match"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, "Input text and output checkbox is not equal"
+
